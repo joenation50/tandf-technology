cefa9322b216a9f642b3be42df36ac53adbec40d
Joseph <joenation98@gmail.com>
2026-09-21 17:44:00 +0100

    Simplify App component by removing routes
    Removed authentication and admin routes for testing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
-import Admin from './pages/Admin';
-import Login from './pages/Login';
-import { handleGoogleRedirect } from './lib/googleAuth';
-
-// handleGoogleRedirect();   // <-- temporarily disabled to test for crash
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={
-            <ProtectedRoute>
-              <Admin />
-            </ProtectedRoute>
-          } />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
